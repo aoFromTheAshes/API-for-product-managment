bba313dbb2cfd72448d0976a39a9de1fbbd0f099
aoFromTheAshes <vchepurko0@gmail.com>
2024-10-11 11:52:35 +0300

    register working

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,42 +1,13 @@
-from typing import Optional
-
 from fastapi_users import schemas
-from pydantic import BaseModel
 
-class RoleRead(BaseModel):
-    id: int
-    name: str
-    permissions: Optional[dict] = None
-
-    class Config:
-        orm_mode = True
 
 class UserRead(schemas.BaseUser[int]):
-    id: int
-    email: str
     username: str
-    is_active: bool = True
-    is_superuser: bool = False
-    is_verified: bool = False
-    role: Optional[RoleRead]  # Зв'язок з роллю
 
-    class Config:
-        orm_mode = True
-        
+
 class UserCreate(schemas.BaseUserCreate):
     username: str
-    email: str
-    password: str
-    role_id: Optional[int]  # Тут передається id ролі
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
-    
+
+
 class UserUpdate(schemas.BaseUserUpdate):
-    username: Optional[str] = None
-    email: Optional[str] = None
-    password: Optional[str] = None
-    role_id: Optional[int] = None  # id ролі для оновлення
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
+    username: str
